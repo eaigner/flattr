bb7438e3486c4607b51064dba830451acbd7893a
Erik Aigner <erik@chocomoko.com>
2010-08-19 22:51:29 +0200

    removed test app target

--- obj-c/src/FLToken.h
+++ obj-c/src/FLToken.h
@@ -11,14 +11,24 @@
 #import "OAToken.h"
 
 
+typedef enum {
+	kFLReadAccessLevelMask = 0x0, // we require at least read level so we start at 0
+	kFLPublishAccessLevelMask = 0x1,
+	kFLClickAccessLevelMask = 0x2
+} FLAccessLevel;
+
 @class OAConsumer;
 
 @interface FLToken : OAToken {
 
 }
 
+/*
+ * Token is requested synchronously for GCD async_dispatch(..)
+ * convenience. Feel free to add asynchronous methods.
+ */
 + (FLToken *)requestTokenForConsumer:(OAConsumer *)consumer error:(NSError **)err;
 
-- (BOOL)authorize;
+- (void)openAuthorizationPageRequiringAccessLevel:(FLAccessLevel)level;
 
 @end
--- obj-c/src/FLToken.m
+++ obj-c/src/FLToken.m
@@ -25,15 +25,15 @@
 	// Init data fetcher for token endpoint
 	NSURL *endpoint = [NSURL URLWithString:@"http://api.flattr.com/oauth/request_token"];
 	FLDataFetcher *fetcher = [[FLDataFetcher alloc] initWithEndpoint:endpoint
-															consumer:consumer
-															   token:nil
-															  method:@"POST"];
+																													consumer:consumer
+																														 token:nil
+																														method:@"POST"];
 	
 	// Fetch and validate reponse
 	FLResponse *response = [fetcher fetchSynchronous];
 	if ([response isValid]) {
 		NSString *responseBody = [[NSString alloc] initWithData:response.responseData
-													   encoding:NSUTF8StringEncoding];
+																									 encoding:NSUTF8StringEncoding];
 		
 		FLToken *token = [[FLToken alloc] initWithHTTPResponseBody:responseBody];
 		
@@ -46,8 +46,31 @@
 	return nil;
 }
 
-- (BOOL)authorize {
-	return NO;
+- (void)openAuthorizationPageRequiringAccessLevel:(FLAccessLevel)level {
+	// Build the access scope parameter. Use 'read' as default.
+	NSMutableArray *scopeParams = [NSMutableArray	array];
+	[scopeParams addObject:@"read"];
+	
+	if (level & kFLPublishAccessLevelMask == kFLPublishAccessLevelMask) {
+		[scopeParams addObject:@"publish"];
+	}
+	if (level & kFLClickAccessLevelMask == kFLClickAccessLevelMask) {
+		[scopeParams addObject:@"click"];
+	}
+	
+	NSString *scope = [scopeParams componentsJoinedByString:@","];
+	
+	// Append params to authorization endpoint
+	NSString *params = [NSString stringWithFormat:@"oauth_token=%@&access_scope=%@",
+											self.key,
+											scope];
+	
+	NSString *endpoint = [NSString stringWithFormat:
+												@"https://api.flattr.com/oauth/authenticate?%@",
+												params];
+	
+	// Open the auth page
+	[[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:endpoint]];
 }
 
 @end
